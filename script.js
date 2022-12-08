0612690af7e87ce6da7b1fd7a1d34061e2c0c2d0
Lauren Noble Gutierrez <95086147+lauble@users.noreply.github.com>
2022-12-08 21:07:04 +0000

    added box to store previous color

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const refreshButton = document.getElementById("refresh-button");
 const colorCode = document.querySelector("#color-code");
 const alert = document.querySelector("#alert");
 const copyButton = document.querySelector("#copy-button");
+const prevColor = document.querySelector('#prev-color');
+
+let colorArray = [];
 
 const setBackgroundColor = () => {
-	const randomColor = Math.floor(Math.random()*16777215).toString(16);
+	const randomColor = Math.floor(Math.random() * 16777215).toString(16);
 	return randomColor;
 }
-
 
 const setAlert = () => {
 	alert.innerHTML = "copied to clipboard!"
@@ -23,7 +25,7 @@ const resetAlert = () => {
 const copyToClipboard = () => {
 	// colorCode.focus();
 	colorCode.select();
-  document.execCommand("copy");
+	document.execCommand("copy");
 	setAlert();
 	resetAlert();
 }
@@ -32,10 +34,15 @@ button.addEventListener("click", () => {
 	const randomColor = "#" + setBackgroundColor();
 	document.body.style.backgroundColor = randomColor;
 	colorCode.innerHTML = randomColor;
+	colorArray.push(randomColor)
+	prevColor.innerHTML = colorArray.slice(-2, -1);
+	prevColor.style.backgroundColor = prevColor.innerHTML;
 });
 
 refreshButton.addEventListener("click", () => {
 	document.body.style.backgroundColor = "#ffff";
+	colorCode.innerHTML = "";
+	prevColor.innerHTML = "";
 });
 
 copyButton.addEventListener("click", copyToClipboard);
